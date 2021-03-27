3d8e6f598d257adc152acb14a9c46e84b67142fa
ZX-11 <63094790+ZX-11@users.noreply.github.com>
2021-03-27 21:17:56 +0800

    Update algorithm.hpp

--- libzx/algorithm.hpp
+++ libzx/algorithm.hpp
@@ -4,6 +4,26 @@
 namespace libzx {
 
 template<typename T>
+concept comparable = requires(T t) { t <=> t; };
+
+template<comparable T>
+void sort(slice<T> s) {
+    if (s.size() == 0) return;
+    auto p = &s.front(), q = &s.back();
+    auto&& key = s.front();
+    while (p <= q) {
+        while (*p < key) p++;
+        while (*q > key) q--;
+        if (p <= q) {
+            std::swap(*p, *q);
+            p++, q--;
+        }
+    }
+    if (q > &s.front()) sort(slice<T>(s.begin(), q+1));
+    if (p < &s.back()) sort(slice<T>(p, s.end()));
+}
+
+template<typename T>
 void sort(slice<T> s, std::function<bool(T&,T&)> less = std::less<T>()) {
     if (s.size() == 0) return;
     auto p = &s.front(), q = &s.back();
